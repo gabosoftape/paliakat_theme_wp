f7bf5dd2259bd088d9a8a61183897a4d38754957
gabriel pabon <gabosoft.ape@gmail.com>
2019-04-08 19:01:52 -0500

    update slider

--- page-templates/paliakat.php
+++ page-templates/paliakat.php
@@ -4,7 +4,7 @@ Template Name: Paliakat
 Template Post Type: page
 */
 get_header(); ?>
-
+<?php echo do_shortcode('[smartslider3 slider=2]'); ?>
 <h1>Hola , soy un request de gabriel :p </h1>
 <?php
 get_footer();
