6a41399fa96a637b3f30366a3e97ae41b9206720
gabriel pabon <gabosoft.ape@gmail.com>
2019-04-08 20:23:29 -0500

    update format template paliakat

--- page-templates/paliakat.php
+++ page-templates/paliakat.php
@@ -5,12 +5,10 @@ Template Post Type: page
 */
 get_header(); ?>
 <?php echo do_shortcode('[smartslider3 slider=2]'); ?>
-<h3>Hola , soy un request de gabriel :p </h3>
 <?php
 $params = array('posts_per_page' => 3, 'post_type' => 'product');
 $wc_query = new WP_Query($params);
 ?>
-<ul>
      <?php if ($wc_query->have_posts()) : ?>
     <div class="row">
      <?php while ($wc_query->have_posts()) :
@@ -34,11 +32,8 @@ $wc_query = new WP_Query($params);
      <?php endwhile; ?>
      <?php wp_reset_postdata(); ?>
      <?php else:  ?>
-     <li>
           <?php _e( 'No Products' ); ?>
-     </li>
    </div>
      <?php endif; ?>
-</ul>
 <?php
 get_footer();
