cc8d5c0ab91f3d35ba21f1c96459a4b33258f36c
gabriel pabon <gabosoft.ape@gmail.com>
2019-04-08 19:41:47 -0500

    update design to products posts

--- page-templates/paliakat.php
+++ page-templates/paliakat.php
@@ -7,19 +7,29 @@ get_header(); ?>
 <?php echo do_shortcode('[smartslider3 slider=2]'); ?>
 <h3>Hola , soy un request de gabriel :p </h3>
 <?php
-$params = array('posts_per_page' => 5, 'post_type' => 'product');
+$params = array('posts_per_page' => 3, 'post_type' => 'product');
 $wc_query = new WP_Query($params);
 ?>
-<?php if ($wc_query->have_posts()) : ?>
-<?php while ($wc_query->have_posts()) :
+<ul>
+     <?php if ($wc_query->have_posts()) : ?>
+     <?php while ($wc_query->have_posts()) :
                 $wc_query->the_post(); ?>
-<?php the_title(); ?>
-<?php endwhile; ?>
-<?php wp_reset_postdata(); ?>
-<?php else:  ?>
-<p>
-     <?php _e( 'No Products'); ?>
-</p>
-<?php endif; ?>
+     <li>
+          <h3>
+               <a href="<?php the_permalink(); ?>">
+               <?php the_title(); ?>
+               </a>
+          </h3>
+          <?php the_post_thumbnail(); ?>
+          <?php the_excerpt(); ?>
+     </li>
+     <?php endwhile; ?>
+     <?php wp_reset_postdata(); ?>
+     <?php else:  ?>
+     <li>
+          <?php _e( 'No Products' ); ?>
+     </li>
+     <?php endif; ?>
+</ul>
 <?php
 get_footer();
