ba6cfcb9b309882467e277d6351c15f2389f8c49
gabosoft <gabosoft.ape@gmal.com>
2019-04-24 15:23:11 -0500

    update design test

--- page-templates/paliakat.php
+++ page-templates/paliakat.php
@@ -48,16 +48,43 @@ $wc_query = new WP_Query($params);
     <?php endif; ?>
 -->
     <section class="after_products">
+    	<?php if ($wc_query->have_posts()) : ?>
      	<div class="row fila1">
      		<div class="col-xl-12 pwt_title">
      			<h2>este es un titulo h2</h2>
      		</div>
      	</div>
      	<div class="row fila2">
-     		<div class="col-xl-12">
-     			
-     		</div>
+     		<?php while ($wc_query->have_posts()) :
+                $wc_query->the_post(); ?>
+     		<div class="col-lg-4 col-md-4 col-xl-4">
+	       		<div class="pwt_card">
+	         		<div class="pwt_img">
+	           			<?php the_post_thumbnail(); ?>
+	         		</div>
+			       <div class="pwt_dsc">
+				        <div class="pwt_card_title">
+				           <h5>
+				             <?php the_title(); ?>
+				           </h5>
+				        </div>
+				        <div class="pwt_card_dsc">
+				           <p class="card-text">
+				            <?php the_excerpt(); ?>
+				           </p>
+				           <a class="btn pwt_button" href="<?php the_permalink(); ?>">
+				             Descubrir mas.
+				           </a>
+				        </div>
+	       			</div>
+	     		</div>
+   			</div>
+     		<?php endwhile; ?>
      	</div>
+     	<?php wp_reset_postdata(); ?>
+	    <?php else:  ?>
+	          <?php _e( 'No Products' ); ?>
+	    <?php endif; ?>
      	<div class="row fila3">
      		<div class="col-xl-12">
      			<h2>este es ultimo titulo h2</h2>
