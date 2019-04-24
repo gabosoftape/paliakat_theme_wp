9a0f402b58a230776ed0f02e672bf5b8bcc984c4
gabosoft <gabosoft.ape@gmal.com>
2019-04-24 15:31:43 -0500

    update design to theme txt

--- footer.php
+++ footer.php
@@ -11,11 +11,6 @@
 
 </div><!-- #main -->
 </section><!-- section -->
-
-<div class="footer-callout">
-	<?php shapely_footer_callout(); ?>
-</div>
-
 <footer id="colophon" class="site-footer footer bg-dark" role="contentinfo">
 	<div class="container footer-inner">
 		<div class="row">
