3a82c942e01565b8cb7f1778935b02ecbe2f687d
gabosoft <gabosoft.ape@gmal.com>
2019-04-24 14:47:08 -0500

    clear data theme

--- page-templates/paliakat.php
+++ page-templates/paliakat.php
@@ -3,52 +3,50 @@
 Template Name: Paliakat
 Template Post Type: page
 */
-get_header(); ?>
+	get_header();
+ ?>
 <?php echo do_shortcode('[smartslider3 slider=2]'); ?>
 <?php
 $params = array('posts_per_page' => 3, 'post_type' => 'product');
 $wc_query = new WP_Query($params);
-?>
-     <?php if ($wc_query->have_posts()) : ?>
+?> 
+<!-- ################################################ clear code
+<?php if ($wc_query->have_posts()) : ?>
     <div class="pwt_title">
       <h1>Productos</h1>
-      <p>este es un request de gabriel </p>
     </div>
     <div class="row pwt_con">
-     <?php while ($wc_query->have_posts()) :
+     	<?php while ($wc_query->have_posts()) :
                 $wc_query->the_post(); ?>
-     <div class="col-lg-4 col-md-4 col-xl-4">
-       <div class="pwt_card">
-         <div class="pwt_img">
-           <?php the_post_thumbnail(); ?>
-         </div>
-       <div class="pwt_dsc">
-         <div class="pwt_card_title">
-           <h5>
-             <?php the_title(); ?>
-           </h5>
-         </div>
-         <div class="pwt_card_dsc">
-           <p class="card-text">
-            <?php the_excerpt(); ?>
-           </p>
-           <a class="btn pwt_button" href="<?php the_permalink(); ?>">
-             Descubrir mas.
-           </a>
-         </div>
-
-
-       </div>
-
-     </div>
-   </div>
-
-     <?php endwhile; ?>
-     </div>
-     <?php wp_reset_postdata(); ?>
-     <?php else:  ?>
+     	<div class="col-lg-4 col-md-4 col-xl-4">
+       		<div class="pwt_card">
+         		<div class="pwt_img">
+           			<?php the_post_thumbnail(); ?>
+         		</div>
+		       <div class="pwt_dsc">
+			        <div class="pwt_card_title">
+			           <h5>
+			             <?php the_title(); ?>
+			           </h5>
+			        </div>
+			        <div class="pwt_card_dsc">
+			           <p class="card-text">
+			            <?php the_excerpt(); ?>
+			           </p>
+			           <a class="btn pwt_button" href="<?php the_permalink(); ?>">
+			             Descubrir mas.
+			           </a>
+			        </div>
+       			</div>
+     		</div>
+   		</div>
+     	<?php endwhile; ?>
+    </div>
+    <?php wp_reset_postdata(); ?>
+    <?php else:  ?>
           <?php _e( 'No Products' ); ?>
-     <?php endif; ?>
+    <?php endif; ?>
+-->
     <section class="after_products">
       <div class="row">
         <div class="col-lg-12 col-md-12 col-xl-12">
