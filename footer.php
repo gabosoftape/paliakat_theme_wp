99df9c6dafba3cec89583314f672ed57abeef178
gabosoft <gabosoft.ape@gmal.com>
2019-04-25 16:19:50 -0500

    update width

--- footer.php
+++ footer.php
@@ -13,23 +13,23 @@
 </section><!-- section -->
 <footer id="colophon" class="site-footer footer bg-dark" role="contentinfo">
 	<div class="flex-row">
-		<div id="footer_1" class="col-lg-3">
-			<h3>INFORMACION</h3>
+		<div id="footer_1" class="col-lg-2 col-md-2">
+			<h4>INFORMACION</h4>
 			<hr/>
 		</div>
-		<div id="footer_2" class="col-lg-3">
-			<h3>FORMA DE PAGO</h3>
+		<div id="footer_2" class="col-lg-3 col-md-3">
+			<h4>FORMA DE PAGO</h4>
 			<hr/>
 		</div>
-		<div id="footer_3" class="col-lg-3">
+		<div id="footer_3" class="col-lg-2 col-md-2">
 			<h3>CONDICIONES</h3>
 			<hr/>
 		</div>
-		<div id="footer_4" class="col-lg-3">
+		<div id="footer_4" class="col-lg-2 col-md-2">
 			<h3>SUPPORT</h3>
 			<hr/>
 		</div>
-		<div id="footer_5" class="col-lg-3">
+		<div id="footer_5" class="col-lg-3 col-md-3">
 			<h3>FOLLOW</h3>
 			<hr/>
 		</div>
