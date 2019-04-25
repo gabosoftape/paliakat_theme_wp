6bae2d40c19171bc9aed01be6ef5b3fa49a7b427
gabosoft <gabosoft.ape@gmal.com>
2019-04-25 16:15:42 -0500

    UPDATE FOOTER TITLES

--- footer.php
+++ footer.php
@@ -13,24 +13,24 @@
 </section><!-- section -->
 <footer id="colophon" class="site-footer footer bg-dark" role="contentinfo">
 	<div class="flex-row">
-		<div id="footer_1">
-			<h3></h3>
+		<div id="footer_1" class="col-lg-3">
+			<h3>INFORMACION</h3>
 			<hr/>
 		</div>
-		<div id="footer_2">
-			<h3></h3>
+		<div id="footer_2" class="col-lg-3">
+			<h3>FORMA DE PAGO</h3>
 			<hr/>
 		</div>
-		<div id="footer_3">
-			<h3></h3>
+		<div id="footer_3" class="col-lg-3">
+			<h3>CONDICIONES</h3>
 			<hr/>
 		</div>
-		<div id="footer_4">
-			<h3></h3>
+		<div id="footer_4" class="col-lg-3">
+			<h3>SUPPORT</h3>
 			<hr/>
 		</div>
-		<div id="footer_5">
-			<h3></h3>
+		<div id="footer_5" class="col-lg-3">
+			<h3>FOLLOW</h3>
 			<hr/>
 		</div>
 	</div>
