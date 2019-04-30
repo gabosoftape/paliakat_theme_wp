bf79ae273165b3cb16ec5261d1a5b293ee1a5070
gabosoft <gabosoft.ape@gmal.com>
2019-04-30 16:47:08 -0500

    Revert "upate footer "
    This reverts commit f8fc4c2ff0122e5312ef100db49098f4138eabe0.

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 
 </div><!-- #main -->
 </section><!-- section -->
-<footer id="colophon" class="site-footer footer" role="contentinfo">
+<!-- <footer id="colophon" class="site-footer footer" role="contentinfo">
 	<div class="flex-row">
 		<div id="footer_1" class="col-lg-2 col-md-2">
 			<h4>INFORMACION</h4>
@@ -36,7 +36,106 @@
 	</div>
 
 	<a class="btn btn-sm fade-half back-to-top inner-link" href="#top"><i class="fa fa-angle-up"></i></a>
-</footer><!-- #colophon -->
+</footer> #colophon --> 
+    <footer class="clearfix">
+        <div class="container">
+            <div class="footer-top clearfix">
+                <div class="row">
+                  <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+                    <div class="row">
+                    <div class="footer-block col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                      <a href="https://www.affiliatly.com/af-1017673">Programa de afiliados</a>
+                      <a href="/pages/tiendas">Locations</a>
+                      <a href="/pages/about-us/">Nosotros</a>
+                      <a href="/pages/contact-us/">Contacto</a>
+                      <a href="/pages/terms-and-policies">Términos y Condiciones</a>
+                    </div>
+                    <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                      <a href="/pages/faqs">Faq's</a>
+                      <a href="/pages/terms-and-policies">Tiempo de entrega</a>
+                      <a href="/a/wishlist/">Favoritos</a>
+                      <a href="/pages/sitemap">Sitemap</a>
+                  	</div>
+                  	<div class="footer-block hidden-xs col-xs-6 col-sm-4 col-md-4 col-md-2-padding col-lg-4">
+                      <a href="/collections/all-man">Hombres</a>
+                      <a href="/collections/mujer">Mujeres</a>
+                      <a href="/collections/graphic-tees">Niños</a>
+                      <a href="/collections/backpacks">Accesorios</a>
+                      <a href="/collections/descuentos">Descuentos</a>
+                  	</div>
+                  </div>
+                  </div>
+                  <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+                    <div class="row">
+                    <div class="clearfix hidden-md hidden-lg"></div>
+                    <div class="footer-block col-xs-12 col-sm-4 col-md-5 col-lg-5 pagos-cont">
+                        <h6><img src="//cdn.shopify.com/s/files/1/1182/6070/t/8/assets/i-secure.svg?239" alt="secure"> <p>Pago seguro</p></h6>
+                      	<img src="//cdn.shopify.com/s/files/1/1182/6070/t/8/assets/metodos-pago.svg?239" alt="payments">
+                  	</div>
+                  	<div class="footer-block hidden-xs col-xs-4 col-sm-2 col-md-2 col-lg-2">
+                      <h6>Síguenos</h6>
+                      <div class="redes">
+                      <a href="https://www.facebook.com/AcapellaMx/" target="_blank">
+                      	<img src="//cdn.shopify.com/s/files/1/1182/6070/t/8/assets/f-facebook.svg?239" alt="Facebook">
+                      </a>
+                      <a href="https://www.instagram.com/acapellaco/" target="_blank">
+                      	<img src="//cdn.shopify.com/s/files/1/1182/6070/t/8/assets/f-instagram.svg?239" alt="Instagram">
+                      </a>
+                      <!--<a href="https://twitter.com/acapella_mx?lang=es" target="_blank">
+                      	<img src="//cdn.shopify.com/s/files/1/1182/6070/t/8/assets/f-.svg?239" alt="Twitter">
+                      </a> -->
+                      </div>
+                  	</div>
+                    <div class="footer-block col-xs-12 col-sm-6 col-md-5 col-lg-5">
+                        <h6>Suscríbete a nuestro newsletter</h6>
+                      	<form action="https://acapella.us8.list-manage.com/subscribe/post?u=adb840666a5909c0292fc770b&amp;id=1d722db59a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate other-form" target="_blank" novalidate>
+                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email"><input type="submit" value="Enviar" name="subscribe" id="mc-embedded-subscribe" class="button">
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>
+                        </form>
+                        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+                        <script type='text/javascript'>
+                            (function($) {
+                                window.fnames = new Array();
+                                window.ftypes = new Array();
+                                fnames[0] = 'EMAIL';
+                                ftypes[0] = 'email';
+                                fnames[1] = 'FNAME';
+                                ftypes[1] = 'text';
+                                fnames[2] = 'LNAME';
+                                ftypes[2] = 'text';
+                            }(jQuery));
+                            var $mcj = jQuery.noConflict(true);
+                        </script>
+                    </div>
+                    <div class="visible-xs footer-block col-xs-12">
+                      <h6>Síguenos</h6>
+                      <div class="redes">
+                      <a href="https://www.facebook.com/AcapellaMx/" target="_blank">
+                      	<img src="//cdn.shopify.com/s/files/1/1182/6070/t/8/assets/f-facebook.svg?239" alt="Facebook">
+                      </a>
+                      <a href="https://www.instagram.com/acapellaco/" target="_blank">
+                      	<img src="//cdn.shopify.com/s/files/1/1182/6070/t/8/assets/f-instagram.svg?239" alt="Instagram">
+                      </a>
+                      <!--<a href="https://twitter.com/acapella_mx?lang=es" target="_blank">
+                      	<img src="//cdn.shopify.com/s/files/1/1182/6070/t/8/assets/f-.svg?239" alt="Twitter">
+                      </a> -->
+                      </div>
+                  	</div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="footer-down clearfix">
+                <div class="box">
+                    <div class="copy">Copyright ©2019 PALIAKAT</div>
+                </div><div class="boxb text-right"> <img src="//cdn.shopify.com/s/files/1/1182/6070/t/8/assets/acapella-m.svg?239" alt="Acapella"> </div>
+            </div>
+        </div>
+    </footer>
 </div>
 </div><!-- #page -->
 
