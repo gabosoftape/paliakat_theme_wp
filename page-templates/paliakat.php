2de4256300aa62877fecdd741b281313de74e3f7
gabosoft <gabosoft.ape@gmal.com>
2019-05-14 16:08:46 -0500

    resmove test flags

--- page-templates/paliakat.php
+++ page-templates/paliakat.php
@@ -11,15 +11,42 @@ $params = array('posts_per_page' => 3, 'post_type' => 'product');
 $wc_query = new WP_Query($params);
 ?> 
    <section class="after_products">
+    	<?php if ($wc_query->have_posts()) : ?>
      	<div class="row fila1">
-     		<div class="col-xl-12 col-lg-12 pwt_title">
+     		<div class="col-xl-12 pwt_title">
      			<h2>Productos</h2>
      		</div>
      	</div>
      	<div class="row fila2">
-     		<div class="col-xl-12 col-lg-12 pwt_title">
-     			<?php echo do_shortcode('[recent_products]');?>
-     		</div>
+     		<?php while ($wc_query->have_posts()) :
+                $wc_query->the_post(); ?>
+     		<div class="col-lg-4 col-md-4 col-xl-4">
+	       		<div class="pwt_card">
+	         		<div class="pwt_img">
+	           			<?php the_post_thumbnail(); ?>
+	         		</div>
+			       <div class="pwt_dsc">
+				        <div class="row pwt_card_title">
+				           <h5>
+				             <?php the_title(); ?>
+				           </h5>
+				        </div>
+				        <div class="row pwt_card_dsc">
+				           <p class="card-text">
+				            <?php the_excerpt(); ?>
+				           </p>
+				           <a class="btn pwt_button" href="<?php the_permalink(); ?>">
+				             Descubrir mas.
+				           </a>
+				        </div>
+	       			</div>
+	     		</div>
+   			</div>
+     		<?php endwhile; ?>
+     		<?php wp_reset_postdata(); ?>
+		    <?php else:  ?>
+		          <?php _e( 'No Products' ); ?>
+		    <?php endif; ?>
      	</div>
      	<!-- fila 3 --------------------------------------------------------- -->
      	<div class="row fila3">
